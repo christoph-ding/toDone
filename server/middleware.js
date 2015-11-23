13f685e01c1d5279fc1e1eb4a7b956efec372073
Pokegear <christoph.ding@gmail.com>
2015-11-23 15:44:16 -0800

    Defined Server and Database connections.  Made preliminary directory structure. Working on basic front-end now.

---
+++ server/middleware.js
@@ -0,0 +1,15 @@
+// require
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+module.exports = function(app, express) {
+  // routers
+
+
+  // logging
+  app.
+
+  // serve static files
+  app.use(express.static())
+
+};
---
+++ server/server.js
@@ -0,0 +1,24 @@
+// Define server 
+var express = require('express');
+var app = express();
+
+// Connect to server
+app.listen(8000);
+console.log("Listening in on port 8000");
+
+// middleware
+require('./middleware.js')(app, express);
+
+
+// connect to database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(cb) {
+  console.log('connected to db');
+});
+
+// export
+module.exports = app;

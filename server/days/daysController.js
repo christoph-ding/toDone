ca37b239ef7036bec8ab08c0657fe067b07852cb
Pokegear <christoph.ding@gmail.com>
2015-11-27 20:49:28 -0800

    Now saving a day does as intended: new tasks are added to task collection, duplicates not, saves the day after.  Needs more promisification.

--- server/days/daysController.js
+++ server/days/daysController.js
@@ -1,7 +1,6 @@
-var dayModel = require('./dayModel.js')
-var taskModel = require('../tasks/taskModel.js')
-var tasksController = require('../tasks/tasksController.js')
-// var Q = require('q');
+var dayModel = require('./dayModel.js');
+var taskModel = require('../tasks/taskModel.js');
+var tasksController = require('../tasks/tasksController.js');
 
 module.exports = {
 
@@ -9,19 +8,17 @@ module.exports = {
   handleDay: function (req, res) {
     // first extract the tasks, add them to bank
     var currentDay = req.body;
-    tasksController.extract(req, res, currentDay);
-  },
-
-  sayHello: function(req, res, next) {
-    var newDay = new dayModel({
-        Tasks: req.body
+    tasksController.extract(req, res, currentDay, function () {
+      var newDay = new dayModel({
+          Tasks: currentDay
+        })
+      newDay.save(function (err, newDay) {
+        if (err) {
+          res.send(500, err);
+        } else {
+          res.status(newDay).send(newDay.body);
+        }
+      });
     })
-    newDay.save(function (err, newDay) {
-      if (err) {
-        res.send(500, err);
-      } else {
-        res.status(newDay).send(newDay.body);
-      }
-    });
   }
 }
--- server/tasks/tasksController.js
+++ server/tasks/tasksController.js
@@ -4,56 +4,32 @@ var Promise = require('bluebird');
 
 module.exports = {  
   extract: function(req, res, day, next) {
-    
-    var createTask = Q.nbind(taskModel.create, taskModel);
-    var checkTask = Q.nbind(taskModel.find, taskModel);
+    // use Promise.map to handle async calls on tasks in 'day'
 
-    for (var i = 0; i < day.length; i++) {
-      var currentTask = day[i];      
-      // check if the current tasks exists
-      checkTask({name: currentTask.name})
-        .then(function (match) {
-          if (match.length > 0) {
-            console.log('match for', match);            
-          } else {
-            console.log('no match for ', currentTask);
-          }
-        })
-        .fail(function (error) {
-          console.log('error on ', currentTask);          
-        });
-      // var newTask = {
-      //   name: day[i].name,
-      //   frequency: 1,
-      //   weight: day[i].weight
-      // };
-      // createTask(newTask)      
-      // .fail(function (error) {
-      //   console.log('error ', i);
-      // });
-    }
-  }
+    Promise.map(day, function(task) {      
+      // check if the current tasks exists      
+      console.log('current task is ', task.name);
+      taskModel.find({name: task.name} ,function (err, match) {
+        if (err) { // error
+          res.send(500, err);
+        } else if (match.length === 0) {
+          var newTask = new taskModel({
+            name: task.name,
+            frequency: 1,
+            weight: task.weight
+          })
+          newTask.save(function (err, newTask) {
+            if (err) {
+              console.log('there was an error adding ', task.name, ' ... ');                 
+            } else {
+              console.log('there was no error adding ', task.name, ' ... ');               
+            }
+          });
+        }        
+      })
+    }).then(function() {
+      console.log("done...sending response");
+      next();    
+    });
+  }  
 };
-    //   var checkTask = Q.nbind(taskModel.find, taskModel);
-    //   checkTask({})
-    //     .then(function (results) {
-    //       if (results) 
-    //       // if this task does not already exists, add it
-    //       console.log('searching for ', day[i].name, '    found ', results)
-    //       if (results.length === 0) {      
-          // add that task to a task collection
-          //   console.log('adding...  ', day[i].name);
-    //       }
-    //     })
-    //     .fail(function (error) {
-    //       console.log("encountered error! ", i)          
-    //     })
-    // }
-
-      // newTask.save(function (err, newTask) {
-      //   if (err) {
-      //     res.send(500, err);
-      //   } else {
-      //     res.status(newTask).send(newTask.body);
-      //   }
-      // })
